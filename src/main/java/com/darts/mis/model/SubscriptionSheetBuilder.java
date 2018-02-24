ac6f2000325773e3535951ce0cad62e0f719e686
jpcuve <gexcuj01i>
2018-02-24 18:35:18 +0100

    .

--- src/main/java/com/darts/mis/model/SubscriptionSheetBuilder.java
+++ src/main/java/com/darts/mis/model/SubscriptionSheetBuilder.java
@@ -1,17 +1,15 @@
 package com.darts.mis.model;
 
+import com.darts.mis.LocalDateRange;
 import com.darts.mis.domain.Domain;
-import com.darts.mis.domain.SubscriptionEdit;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 import java.time.Year;
 import java.util.Arrays;
-import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -28,12 +26,11 @@ public class SubscriptionSheetBuilder implements SheetBuilder {
     @Override
     public void addSheets(HSSFWorkbook workbook) {
         int year = Year.now().getValue();
-        final LocalDate from = LocalDate.of(year - 1, 1, 1);
-        final LocalDate to = LocalDate.of(year, 1, 1);
+        final LocalDateRange range = LocalDateRange.of(year - 1, 1, 1, year, 1, 1);
         final AtomicInteger row = new AtomicInteger();
         final Sheet sheet = workbook.createSheet("Subscriptions");
 
-        sheet.createRow(row.getAndIncrement()).createCell(0).setCellValue("Subscription revenues from: " + from + " to: " + to);
+        sheet.createRow(row.getAndIncrement()).createCell(0).setCellValue("Subscription revenues for: " + range);
         final Row titleRow = sheet.createRow(row.getAndIncrement());
         final AtomicInteger titleCol = new AtomicInteger();
         Arrays.stream(titles).forEach(title -> titleRow.createCell(titleCol.getAndIncrement()).setCellValue(title));
@@ -41,7 +38,7 @@ public class SubscriptionSheetBuilder implements SheetBuilder {
         for (final AccountItem accountItem: revenueModel.getAccountItems()) {
             for (final SubscriptionItem subscriptionItem: accountItem.getSubscriptionItems()){
                 subscriptionItem.getLastRenewOrUpdate().ifPresent(se -> {
-                    if (!(se.getTo().isBefore(from) || se.getFrom().isAfter(to))){ // TODO change this test to any subscription exists between from and to
+                    if (range.isOverlapping(se.getRange())) {
                         final Row subscriptionRow = sheet.createRow(row.getAndIncrement());
                         final AtomicInteger subscriptionCol = new AtomicInteger();
                         subscriptionRow.createCell(subscriptionCol.getAndIncrement()).setCellValue(accountItem.getAccount().getId());
@@ -56,7 +53,5 @@ public class SubscriptionSheetBuilder implements SheetBuilder {
                 });
             }
         }
-
-
     }
 }
